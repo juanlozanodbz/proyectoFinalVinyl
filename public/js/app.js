808da1a7c3e61626ee04c51c06a044e9bef352f0
RodrigoCravioto <a01748172@itesm.mx>
2021-12-02 08:38:16 -0600

    Javascript separado

--- public/js/app.js
+++ public/js/app.js
@@ -1,107 +1,91 @@
-
-
 var url="http://localhost:8083/albums";
-var modal=new bootstrap.modal(document.getElementById('modelId'), {keyboard:false});
+        var modal = new bootstrap.Modal(document.getElementById('modelId'), {keyboard:false});
 
-var aplicacion= new function(){
+        var aplicacion= new function(){
 
-    this.idEditar= document.getElementById("idEditar");
-    this.nombreEditar= document.getElementById("nombreEditar");
-    this.autorEditar= document.getElementById("autorEditar");
-    this.añoEditar= document.getElementById("añoEditar");
-    this.generoEditar= document.getElementById("generoEditar");
+            this.idBorrar= document.getElementById("idBorrar");
 
-    this.id= document.getElementById("id");
-    this.nombre= document.getElementById("nombre");
-    this.autor= document.getElementById("autor");
-    this.año= document.getElementById("año");
-    this.genero= document.getElementById("genero");
+            this.idActualizar= document.getElementById("idActualizar");
+            this.nombreActualizar= document.getElementById("nombreActualizar");
+            this.autorActualizar= document.getElementById("autorActualizar");
+            this.añoActualizar= document.getElementById("añoActualizar");
+            this.generoActualizar= document.getElementById("generoActualizar");
 
-    this.albumes=document.getElementById("albumes");
+            this.id= document.getElementById("id");
+            this.nombre= document.getElementById("nombre");
+            this.autor= document.getElementById("autor");
+            this.año= document.getElementById("año");
+            this.genero= document.getElementById("genero");
 
-    this.Leer=function(){
-        var datos="";
-        fetch(url+"/getAlbums")
-        .then(r=>r.json())
-        .then((respuesta)=>{
-            console.log(respuesta);
-            respuesta.map(
-                function (album,index,array){
-                    datos+="<tr>";
-                    datos+="<td>"+album.id+"</td>";
-                    datos+="<td>"+album.nombre+"</td>"; 
-                    datos+="<td>"+album.genero+"</td>";
-                    datos+="<td>"+album.año+"</td>";
-                    datos+='<td> <div class="btn-group" role="group" aria-label=""><button type="button" class="btn btn-info" onclick="aplicacion.Editar('+album.id+')">Editar</button><button type="button" class="btn btn-danger" onclick="aplicacion.Borrar('+album.id+')">Borrar</button></div>'+'</td>';
-                    datos+="</tr>";
-                }
-            );
-            return this.albumes.innerHTML=datos;
-        })
-        .catch(console.log);
+            this.albumes=document.getElementById("albumes");
 
-        //datos="<tr><td>1</td><td>Play Deep</td><td>The Outfield</td><td>1985</td><td>Rock</td><td>Editar | Borrar</td></tr>";
-        
-    };
-    this.Agregar=function(){
-        console.log(nombre.value);
-        console.log(autor.value);
-        console.log(año.value);
-        console.log(genero.value);
+            this.Leer=function(){
+                var datos="";
+                fetch(url+"/getAlbums")
+                .then(r=>r.json())
+                .then((respuesta)=>{
+                    console.log(respuesta);
+                    respuesta.map(
+                        function (album,index, array){
+                            datos+="<tr>";
+                            datos+="<td>"+album.id+"</td>";
+                            datos+="<td>"+album.nombre+"</td>";
+                            datos+="<td>"+album.autor+"</td>";  
+                            datos+="<td>"+album.genero+"</td>";
+                            datos+="<td>"+album.año+"</td>";
+                            datos+="</tr>";
+                        }
+                    );
+                    return this.albumes.innerHTML=datos;
+                })
+                .catch(console.log);
+                
+            };
+            this.Agregar=function(){
+                console.log(nombre.value);
+                console.log(autor.value);
+                console.log(año.value);
+                console.log(genero.value);
 
-        var datosEnviar={id:this.id.value, name:this.nombre.value, artist:this.autor.value, year:this.año.value, genre:this.genero.value}
+                var datosEnviar={id:"1", name:this.nombre.value, artist:this.autor.value, year:this.año.value, genre:this.genero.value}
 
-        fetch(url+"/addAlbum", {method:"POST", body:JSON.stringify(datosEnviar)})
-        .then((respuesta)=>respuesta.json())
-        .then((datosDeRespuesta)=>{
-            console.log("insertados");
-            this.Leer();
-        })
-        .catch(console.log);
-    };
+                fetch(url+"/addAlbum", {method:"POST", body:JSON.stringify(datosEnviar)})
+                .then((respuesta)=>respuesta.json())
+                .then((datosDeRespuesta)=>{
+                    console.log("insertados");
+                    console.log(datosEnviar);
+                    this.Leer();
+                })
+                .catch(console.log);
+            };
 
-    this.Borrar=function(id){
-        console.log(id);
-        fetch(url+"?borrar="+id)
-        .then((respuesta)=>respuesta.json())
-        .then((datosDeRespuesta)=>{
-            this.Leer();
-        })
-        .catch(console.log);
-    };
+            this.Borrar=function(idBorrar){
+                var datosBorrar={id:this.idBorrar.value}
+                fetch(url+"/deleteAlbum", {method:"POST", body:JSON.stringify(datosBorrar)})
+                .then((res)=>res.json())
+                .then((datosDeRes)=>{
+                    console.log("borrado");
+                    console.log(datosBorrar);
+                    this.Leer();
+                })
+                .catch(console.log);
+            };
 
-    this.Editar=function(id){
-        console.log(id);
+            
 
-        fetch(url+"?consultar="+id)
-        .then((respuesta)=>respuesta.json())
-        .then((datosDeRespuesta)=>{
-            console.log(datosDeRespuesta);
+            this.Actualizar=function(idActualizar){
 
-            this.idEditar.value=datosDeRespuesta[0]['id'];
-            this.nombreEditar.value=datosDeRespuesta[0]['nombre'];
-            this.autorEditar.value=datosDeRespuesta[0]['autor'];
-            this.añoEditar.value=datosDeRespuesta[0]['año'];
-            this.generoEditar.value=datosDeRespuesta[0]['genero'];
-        })
-        .catch(console.log);
+                var datosActualizar={id:this.idActualizar.value, name:this.nombreActualizar.value, artist:this.autorActualizar.value, year:this.añoActualizar.value, genre:this.generoActualizar.value}
 
-        modal.show();
-    };
+                fetch(url+"/updateAlbum", {method:"POST", body:JSON.stringify(datosActualizar)})
+                .then((respuesta)=>respuesta.json())
+                .then((datosDeRespuesta)=>{
+                    console.log("Actualizados");
+                    console.log(datosActualizar);
+                    this.Leer();
+                })
+                .catch(console.log);
 
-    this.Actualizar=function(){
-
-        var datosEnviar={id:this.idEditar.value, nombre:this.nombreEditar.value, autor:this.autorEditar.value, año:this.añoEditar.value, genero:this.generoEditar.value}
-
-        fetch(url+"?actualizar=1", {method:"POST", body:JSON.stringify(datos)})
-        .then((respuesta)=>respuesta.json())
-        .then((datosDeRespuesta)=>{
-            console.log("Actualizados");
-            this.Leer();
-            modal.hide();
-        })
-        .catch(console.log);
-
-    }
-}
-aplicacion.Leer();
+            }
+        }
+        aplicacion.Leer();
